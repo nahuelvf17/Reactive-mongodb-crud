d6e725f93f433d912f57f4962b66a60da08ca9e4
Nahuel Ovando <nahuel@yahoo.com>
2021-08-29 11:09:08 -0300

    Reactive mongodb crud

---
+++ src/main/java/com/novando/reactive/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.novando.reactive.repository;
+
+import com.novando.reactive.dto.ProductDto;
+import com.novando.reactive.entity.Product;
+import org.springframework.data.domain.Range;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+@Repository
+public interface ProductRepository extends ReactiveMongoRepository<Product, String> {
+    Flux<ProductDto> findByPriceBetween(Range<Double> priceRange);
+}
